6d9edc775dc4722c93e3379d97435e0404c46254
Henry Borchers <henryborchers@yahoo.com>
2016-12-15 14:11:36 -0600

    updated refactored name for the settings file

--- exiv2Driver/config.py
+++ exiv2Driver/config.py
@@ -4,10 +4,5 @@ import shutil
 
 def get_exiv2_path():
     config = configparser.ConfigParser()
-    config.read("settings/exvsettings.ini")
+    config.read("settings/settings.ini")
     return config['commands'].get('exiv2', shutil.which("exiv2"))
-
-
-# try:
-# except KeyError:
-#     EXIV2_PATH = shutil.which("exiv2")
--- setup.py
+++ setup.py
@@ -1,19 +1,19 @@
 from setuptools import setup
 
 setup(
-    name='exiv2Driver',
-    version='0.0.1a0',
+    name='exiv2CommandBuilder',
+    version='0.0.1b0',
     packages=['exiv2Driver'],
     scripts=['scripts/dummy.py'],
     test_suite="tests",
     tests_require=['pytest'],
     data_files=[
-        ('settings', ['settings/exvsettings.ini']),
+        ('settings', ['settings/settings.ini']),
                 ],
     zip_safe=False,
     url='',
     license='',
     author='Henry Borchers',
     author_email='hborcher@illinois.edu',
-    description='Wrapper around exiv2'
+    description='Command builder for exiv2'
 )
